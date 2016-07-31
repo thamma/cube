07a3736a98c5cfff2d4be1e64d3f60dc3f4a27de
Dominic Zimmer <s8dozimm@stud.uni-saarland.de>
2016-07-31 13:09:13 +0200

    added turn tests, debugged translations

--- tests/TestAlgorithmCollection.java
+++
@@ -1,5 +0,0 @@
-import me.thamma.cube.model.Algorithm;
-import me.thamma.cube.model.Turn;
-
-public class TestAlgorithmCollection {
-}
--- tests/TurnTests.java
+++ tests/TurnTests.java
@@ -1,11 +1,54 @@
 import me.thamma.cube.model.Turn;
 import org.junit.Test;
 
+import static junit.framework.TestCase.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 public class TurnTests {
 
     @Test
-    public void turnInvariants() {
-        for (Turn t : Turn.values()) {
-        }
+    public void turnSelfinverse() {
+        for (Turn t : Turn.values())
+            assertTrue(t.inverse().inverse() == t);
     }
+
+    @Test
+    public void turnWides() {
+        for (Turn t : Turn.values())
+            assertTrue(t.name().toLowerCase().contains("wide") == t.isWideTurn());
+    }
+
+    @Test
+    public void turnSlice() {
+        for (Turn t : Turn.values())
+            assertTrue((t.name().startsWith("M") || t.name().startsWith("E") || t.name().startsWith("S")) == t.isSliceTurn());
+    }
+
+    @Test
+    public void turnRotations() {
+        for (Turn t : Turn.values())
+            assertTrue((t.name().contains("X") || t.name().contains("Y") || t.name().contains("Z")) == t.isCubeRotation());
+    }
+
+    @Test
+    public void turnTranslationsXY() {
+        Turn t1 = Turn.X, t2 = Turn.Y;
+        for (Turn t : Turn.values())
+            assertTrue(String.format("Invariant broke for Turn %s\n", t), t.translateTurn(t1).translateTurn(t2).translateTurn(t1).translateTurn(t2).translateTurn(t1).translateTurn(t2) == t);
+    }
+
+    @Test
+    public void turnTranslationsXZ() {
+        Turn t1 = Turn.X, t2 = Turn.Z;
+        for (Turn t : Turn.values())
+            assertTrue(String.format("Invariant broke for Turn %s\n", t),t.translateTurn(t1).translateTurn(t2).translateTurn(t1).translateTurn(t2).translateTurn(t1).translateTurn(t2) == t);
+    }
+
+    @Test
+    public void turnTranslationsYZ() {
+        Turn t1 = Turn.Y, t2 = Turn.Z;
+        for (Turn t : Turn.values())
+            assertTrue(String.format("Invariant broke for Turn %s\n", t),t.translateTurn(t1).translateTurn(t2).translateTurn(t1).translateTurn(t2).translateTurn(t1).translateTurn(t2) == t);
+    }
+
 }
