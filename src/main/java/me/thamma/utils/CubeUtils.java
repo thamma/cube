d8a10b8135f683398df5d336c30a0ce8df4b7ff5
Dominic Zimmer <s8dozimm@stud.uni-saarland.de>
2016-08-27 20:59:38 +0200

    added naive breadth search

--- src/main/java/me/thamma/utils/CubeUtils.java
+++ src/main/java/me/thamma/utils/CubeUtils.java
@@ -4,11 +4,13 @@ import me.thamma.cube.algorithmInterpreter.Interpreter;
 import me.thamma.cube.algorithmInterpreter.lexer.IllegalCharacterException;
 import me.thamma.cube.algorithmInterpreter.parser.expressions.Exceptions.UnexpectedEndOfLineException;
 import me.thamma.cube.algorithmInterpreter.parser.expressions.Exceptions.UnexpectedTokenException;
-import me.thamma.cube.model.Algorithm;
-import me.thamma.cube.model.Cube;
-import me.thamma.cube.model.CubeConstants;
-import me.thamma.cube.model.Sticker;
+import me.thamma.cube.model.*;
 import me.thamma.utils.solverModel.Search;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+import static me.thamma.cube.model.Turn.*;
 
 public class CubeUtils {
 
@@ -50,6 +52,40 @@ public class CubeUtils {
             solution = Search.solution(cube.normalizeRotation().getFaceletDefinition(CubeConstants.Stickers.alienFaceletDefinition), i, 12, false);
         }
         return Algorithm.fromScramble(solution);
+    }
+
+    public static Algorithm bfsSolution(Cube cube) {
+        return bfsSolution(cube, new Turn[]{UP, FRONT, RIGHT, DOWN, BACK, LEFT});
+    }
+
+    public static Algorithm bfsSolution(Cube cube, Turn[] turns) {
+        Queue<Algorithm> solutions = new LinkedList<>();
+        solutions.add(new Algorithm());
+        for (Turn t : turns)
+            solutions.add(new Algorithm(t));
+
+        int currSize = 1;
+        long start = System.nanoTime();
+        double lastdelta = 1;
+        while (!solutions.isEmpty()) {
+
+            Algorithm head = solutions.poll();
+            if (head.size() > currSize) {
+                currSize++;
+                System.out.println(head);
+                System.out.printf("%d\t%.2f\t%.1f\n", currSize, Double.valueOf(System.nanoTime() - start)/1000000000, (System.nanoTime() - start)/lastdelta/1000000000);
+                lastdelta =  Double.valueOf(System.nanoTime() - start)/1000000000;
+                start = System.nanoTime();
+            }
+            if (cube.clone().turn(head).isSolved())
+                return head;
+            for (Turn t : turns) {
+                Algorithm temp = head.clone().concat(t).cancelOut();
+                if (temp.size() > currSize)
+                    solutions.add(temp);
+            }
+        }
+        return null;
     }
 
     public static Algorithm anySolve(Cube cube) {
